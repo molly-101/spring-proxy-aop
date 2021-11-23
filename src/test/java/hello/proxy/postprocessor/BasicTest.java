548c92157b35932d31f0c45f7a721123dedd37e8
onAuspicious <wotj7687@icloud.com>
2021-11-23 15:59:41 +0900

    빈 후 처리기 BeanPostProcessor 로 Spring Container 에 다른 객체를 주입해주자!

---
+++ src/test/java/hello/proxy/postprocessor/BasicTest.java
@@ -0,0 +1,56 @@
+package hello.proxy.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+public class BasicTest {
+
+    @Test
+    void basicConfig() {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BasicConfig.class);
+
+        // A는 빈으로 등록된다.
+        A a = applicationContext.getBean("beanA", A.class);
+        a.helloA();
+
+        // B 는 빈으로 등록되지 않는다.
+        Assertions.assertThrows(NoSuchBeanDefinitionException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                applicationContext.getBean(B.class);
+            }
+        });
+    }
+
+
+    @Slf4j
+    @Configuration
+    static class BasicConfig {
+        @Bean(name = "beanA")
+        public A a() {
+            return new A();
+        }
+    }
+
+    @Slf4j
+    static class A {
+        public void helloA() {
+            log.info("hello A");
+        }
+    }
+
+    @Slf4j
+    static class B {
+        public void helloA() {
+            log.info("hello B");
+        }
+    }
+}
